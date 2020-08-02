dc1c715b13da5c1b1bcad8a7b6a5bf214208a4ec
Luis Carlin <luiscarlin@gmail.com>
2020-08-01 22:02:41 -0400

    clean up

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,25 +1,17 @@
 import { Sequelize } from "sequelize-typescript";
 require("dotenv").config();
 
-const {
-  DB_CONNECTION_DB,
-  DB_CONNECTION_USERNAME,
-  DB_CONNECTION_PASSWORD,
-  DB_CONNECTION_HOST,
-} = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize({
-  database: DB_CONNECTION_DB,
-  username: DB_CONNECTION_USERNAME,
-  password: DB_CONNECTION_PASSWORD,
-  host: DB_CONNECTION_HOST,
+  database: DB_NAME,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: 5000,
   models: [__dirname + "/models"],
   dialect: "postgres",
   protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-  logging: false,
 });
+
+export { sequelize };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { sequelize } from "./db";
 
 require("dotenv").config();
 
 const app = express();
 const { SERVER_PORT } = process.env;
 
-app.use(express.json());
-app.listen(SERVER_PORT, () => console.log(`Listening at port ${SERVER_PORT}`));
+// app.use(express.json());
+app.listen(SERVER_PORT, () =>
+  sequelize
+    .authenticate()
+    .then(() => console.log(`Server and DB running at port ${SERVER_PORT}`))
+    .catch((err) => console.log("Connection Failed", err))
+);
