1c8315d57877700b79139b69b805ccf6569fafeb
Luis Carlin <luiscarlin@gmail.com>
2020-08-04 10:46:36 -0400

    setup the tables and make sure they work

--- database/init-db.sh
+++ database/init-db.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+echo "DB_USER: ${DB_USER} DB_PASSWORD: ${DB_PASSWORD} DB_NAME: ${DB_NAME} POSTGRES_PASSWORD: ${POSTGRES_PASSWORD} POSTGRES_USER: ${POSTGRES_USER} POSTGRES_DB: ${POSTGRES_DB}"
+
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB <<-EOSQL
   CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';
   CREATE DATABASE $DB_NAME;
   GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_PASSWORD" <<-EOSQL
-  DROP TABLE IF EXISTS user;
-  DROP TABLE IF EXISTS event;
-EOSQL
-
-psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_PASSWORD" <<-EOSQL
-  CREATE TABLE user (
+psql -v ON_ERROR_STOP=1 --username $DB_USER --dbname $DB_NAME <<-EOSQL
+  CREATE TABLE public.user (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
@@ -23,8 +20,8 @@ psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_PASSWORD" <<-EOSQL
   );
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_PASSWORD" <<-EOSQL
-  CREATE TABLE event (
+psql -v ON_ERROR_STOP=1 --username $DB_USER --dbname $DB_NAME <<-EOSQL
+  CREATE TABLE public.event (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES "user"(id),
     title VARCHAR NOT NULL,
